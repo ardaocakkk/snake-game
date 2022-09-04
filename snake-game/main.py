13205da2856434898fee488e4092c0ca5fe9a33e
ardaocakkk <109307635+ardaocakkk@users.noreply.github.com>
2022-09-04 15:30:22 +0300

    Update main.py

--- snake-game/main.py
+++ snake-game/main.py
@@ -32,7 +32,7 @@ while is_game_on:
         snake.extend()
         scoreboard.increase_score()
 
-    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 300 or snake.head.ycor() < -300:
         is_game_on = False
         scoreboard.game_over()
         
